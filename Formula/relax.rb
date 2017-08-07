5aaac66f4792d943030ab0d4a32e3bd8b342fdc0
Shin Yamamoto <shin@scenee.com>
2017-08-07 18:35:57 +0900

    Release relax v0.3.7

--- Formula/relax.rb
+++ Formula/relax.rb
@@ -1,8 +1,8 @@
 class Relax < Formula
   desc "Release tool for Xcode"
   homepage "https://github.com/SCENEE/relax"
-  url "https://github.com/SCENEE/relax/archive/v0.3.6.tar.gz"
-  sha256 "20b8f8f397a1cff4ad5d2a4f2fc34e681c60a8113f988045d76c2eaf559dbea8"
+  url "https://github.com/SCENEE/relax/archive/v0.3.7.tar.gz"
+  sha256 "ae5ca53dfbeaaca04afecbcadf488d16ab91f3b326d759c7832f79ed13db058d"
   head "https://github.com/SCENEE/relax.git"
 
   def install
