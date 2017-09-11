021395927557c05d0d6cfe2e3c860ea93a79b399
Shin Yamamoto <shin@scenee.com>
2017-09-11 15:20:51 +0900

    Release relax v0.5.0

--- Formula/relax.rb
+++ Formula/relax.rb
@@ -1,13 +1,13 @@
 class Relax < Formula
   desc "Release tool for Xcode"
   homepage "https://github.com/SCENEE/relax"
-  url "https://github.com/SCENEE/relax/archive/v0.3.7.tar.gz"
-  sha256 "ae5ca53dfbeaaca04afecbcadf488d16ab91f3b326d759c7832f79ed13db058d"
+  url "https://github.com/SCENEE/relax/archive/v0.5.0.tar.gz"
+  sha256 "466e83f737053e1b583f96a98783488557a836a934c92dae462180800beb318e"
   head "https://github.com/SCENEE/relax.git"
 
   def install
     prefix.install "LICENSE", "README.md"
-    prefix.install Dir["bin", "libexec", "completions"]
+    prefix.install Dir["bin", "libexec", "go", "completions"]
   end
 
   def caveats; <<-EOS.undent
