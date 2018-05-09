9f1f68c76be9735e2faf510c21b04d6c7e91de04
Shin Yamamoto <shin@scenee.com>
2018-05-09 09:20:09 +0900

    Release relax v0.6.0

--- Formula/relax.rb
+++ Formula/relax.rb
@@ -1,18 +1,25 @@
 class Relax < Formula
-  desc "Release tool for Xcode"
+  desc "Lazy release tool for iOS developers"
+
   homepage "https://github.com/SCENEE/relax"
-  url "https://github.com/SCENEE/relax/archive/v0.5.7.tar.gz"
-  sha256 "154cd42830b6cfe1f3e423e6d2ffefe075a217f9325b24a010d5916c7e54977f"
+
+  url "https://github.com/SCENEE/relax/archive/v0.6.0.tar.gz"
+  sha256 "00515f71c319bc4c39ac1df0bfdd4fa857e6a8454353ebc2d9649f527702aab2"
+
   head "https://github.com/SCENEE/relax.git"
 
   def install
     prefix.install "LICENSE", "README.md"
-    prefix.install Dir["bin", "libexec", "go", "completions"]
+    prefix.install Dir["bin", "libexec", "go", "completions", "share"]
   end
 
   def caveats; <<~EOS
     To enable completion, add the following line to your bash profile:
       if which relax > /dev/null; then source "$(relax init completion)"; fi
 EOS
+  end
+
+  test do
+    system "${bin}/relax", "--version"
   end
 end
